f2146fff289404cff93231f8062b7869be6a2d1a
Natalia  Wit <natwit95@gmail.com>
2019-12-29 17:42:01 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -51,7 +51,7 @@ def matrix_lookup(matrix, row, column)
   ["Grapefruit", "Pineapple", "Oranges", "Watermelon","Eggplant"],
   ["Strawberries", "Potatoes", "Grapes", "Avocadoes", "Asparagus"]
 ]
-  produce_storage_room[1][1]
+  matrix_lookup[1][1]
 
 end
 
