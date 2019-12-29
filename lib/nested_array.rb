6077d5a7b9d6821a36ee884f6e14b48952d8442c
Natalia  Wit <natwit95@gmail.com>
2019-12-29 16:58:37 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -23,8 +23,8 @@ def assembled_matrix
   # Make sure conventional produce is first, on the 'zeroth' / 'bottom' shelf
 
   produce_storage_room = [
-  ["Grapefruit", "Pineapple", "Oranges", "Watermelon","Eggplant"]  
-  ["Strawberries", "Potatoes", "Grapes", "Avocadoes", "Asparagus"],
+  ["Grapefruit", "Pineapple", "Oranges", "Watermelon","Eggplant"], 
+  ["Strawberries", "Potatoes", "Grapes", "Avocadoes", "Asparagus"]
 
 ]
 end
