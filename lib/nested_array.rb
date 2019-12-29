db0cb9d13dd8590a602863e6c6294a2b61719918
Natalia  Wit <natwit95@gmail.com>
2019-12-29 18:09:50 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -63,7 +63,7 @@ def matrix_update(matrix, row, column, new_value)
   ["Grapefruit", "Pineapple", "Oranges", "Watermelon","Eggplant"],
   ["Strawberries", "Potatoes", "Grapes", "Avocadoes", "Asparagus"]
   ]
-  matrix[1][1]
+  
   matrix[1][1] = "love"
-  puts matrix[1]
+  matrix[1]
 end
